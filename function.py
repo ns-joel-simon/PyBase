64f93bff43119b34033b2fa009c9db004c12b48e
joshua <joshua.emptyslate@gmail.com>
2024-10-03 16:53:32 +0530

    oct 3

--- function.py
+++ function.py
@@ -8,12 +8,12 @@
 # print("joe")
 # # printme ("j'ai")
 
-def testfunction(arg):
-   print ("ID inside the function:", id(arg))
-   arg = arg + 1
-   print ("new object after increment", arg, id(arg))
-
-var=10
-print ("ID before passing:", id(var))
-testfunction(var)
-print ("value after function call", var)
+# def testfunction(arg):
+#    print ("ID inside the function:", id(arg))
+#    arg = arg + 1
+#    print ("new object after increment", arg, id(arg))
+#
+# var=10
+# print ("ID before passing:", id(var))
+# testfunction(var)
+# print ("value after function call", var)
